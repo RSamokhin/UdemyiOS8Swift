4d26654ac7999e2527225ba82850df574d139179
rSamokhin <poshliemail@googlemail.com>
2015-07-09 11:45:22 +0300

    println and lable text changed

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var myLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        myLabel.text = "New labele text";
+        
+        println("Hello world!");
     }
 
     override func didReceiveMemoryWarning() {
