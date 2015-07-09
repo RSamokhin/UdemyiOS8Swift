28214ad519f32696ee35e5e44702cd8a191728fb
rSamokhin <poshliemail@googlemail.com>
2015-07-09 14:33:54 +0300

    playgrounds

--- Playgrounds/MyPlayground.playground/Contents.swift
+++ Playgrounds/MyPlayground.playground/Contents.swift
@@ -3,3 +3,34 @@
 import UIKit
 
 var str = "Hello, playground"
+
+str = "Goodbye"
+
+var name:String = "Dasha"
+
+name = "Roman"
+
+var unknownString:String
+
+unknownString = "3"
+
+var int = 4
+
+3*int
+
+var a:Int = 5
+var b = 5 * 3
+var c:Double = 5/2
+
+c * 2
+
+c * Double(int)
+
+var longSentence = str + " " + name
+
+var newString = "instert var in string: \(unknownString)"
+
+var q = 3.5
+var w = 4
+var e = q * Double(w)
+var result = "the \(q) * \(w) = \(e) "
